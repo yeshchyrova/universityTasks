cd097d411b8c2c065994e9c772c234adc06cb2f5
yeshchyrova <e.shchyrova@gmail.com>
2024-09-30 08:49:58 +0200

    Add solution to Time Machine task

--- src/dynamic_programming/Main.java
+++ src/dynamic_programming/Main.java
@@ -1,13 +1,18 @@
-package dynamic_programming;
+//package dynamic_programming;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//
+////  -------------------------------- TASK 1 --------------------------------
+////    int n = 64;
+////    BinaryCodeSequence bcs = new BinaryCodeSequence(n);
+////    long res = bcs.getAmountOfSequences(n);
+////    System.out.println(res);
+////    System.out.println("amount of calls: " + bcs.getAmountOfCalls());
+//
+//
+//
+////  -------------------------------- TASK 2 --------------------------------
 
-public class Main {
-  public static void main(String[] args) {
-
-//  -------------------------------- TASK 1 --------------------------------
-    int n = 64;
-    BinaryCodeSequence bcs = new BinaryCodeSequence(n);
-    long res = bcs.getAmountOfSequences(n);
-    System.out.println(res);
-    System.out.println("amount of calls: " + bcs.getAmountOfCalls());
-  }
-}
+//  }
+//}
---
+++ src/dynamic_programming/TimeMachine.java
@@ -0,0 +1,53 @@
+package dynamic_programming;
+
+import java.util.*;
+
+public class TimeMachine {
+  private static int[] minOperations;
+  private static int[] fromWhere;
+  private static int n = 1; // years
+
+
+  public static void createDynamicBase() {
+    minOperations = new int[n + 1];
+    minOperations[1] = 0;
+    fromWhere = new int[n + 1];
+    fromWhere[1] = 0;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+
+    createDynamicBase();
+
+    for (int i = 2; i <= n; i++) {
+      fromWhere[i] = i - 1;
+      minOperations[i] = minOperations[i - 1] + 1;
+
+      if (i % 2 == 0 && minOperations[i / 2] + 1 < minOperations[i]) {
+        fromWhere[i] = i / 2;
+        minOperations[i] = minOperations[i / 2] + 1;
+      }
+
+      if (i % 3 == 0 && minOperations[i / 3] + 1 < minOperations[i]) {
+        fromWhere[i] = i / 3;
+        minOperations[i] = minOperations[i / 3] + 1;
+      }
+    }
+    System.out.println(minOperations[n]);
+    List<Integer> result = new ArrayList<>();
+
+    for (int i = n; i > 0; ) {
+      result.add(i);
+      i = fromWhere[i];
+    }
+
+    for (int i = result.size() - 1; i > 0; i--) {
+      System.out.print(result.get(i) + " ");
+    }
+    System.out.println(result.get(0));
+
+
+  }
+}
