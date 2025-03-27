0fc8c230b3666110b07d42f4083bc5db9c8efc2a
yeshchyrova <e.shchyrova@gmail.com>
2025-03-27 09:56:02 +0100

    Update lca logic (works correct but time optimization is required)

--- src/lca_rmq/LowestCommonAncestor.java
+++ src/lca_rmq/LowestCommonAncestor.java
@@ -8,52 +8,62 @@ public class LowestCommonAncestor {
   private final int[] depth;
   private final boolean[] isDepthCounted;
   private int LOG;
-  private final int[] requestsData;
-  private final int[] requestAnswers;
   private static int x, y, z, n, m;
+  private long result;
 
   public LowestCommonAncestor(int n, int[] parent) {
-    requestsData = new int[m * 2];
-    requestAnswers = new int[m + 1];
+
     LOG = 0;
     while ((1 << LOG) <= n) {
       LOG++;
     }
+
     isDepthCounted = new boolean[n];
     Arrays.fill(isDepthCounted, false);
-    isDepthCounted[0] = true;
-
     up = new int[n][LOG];
     depth = new int[n];
+
+    up[0][0] = 0;
+    isDepthCounted[0] = true;
     depth[0] = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
       up[i][0] = parent[i];
-      if (i != 0) {
-//        depth[i] = depth[parent[i]] + 1;
-        depth[i] = countDepth(i, parent);
-        System.out.println("depth[" + i + "] = " + depth[i]);
-      }
-      for (int j = 1; j < LOG; j++) {
-        up[i][j] = up[up[i][j - 1]][j - 1];
+    }
+
+    computeDepths(parent);
+
+    for (int j = 1; j < LOG; j++) {
+      for (int i = 0; i < n; i++) {
+        if (up[i][j - 1] != 0) {
+          up[i][j] = up[up[i][j - 1]][j - 1];
+        } else {
+          up[i][j] = 0;
+        }
       }
     }
   }
 
-  int countDepth(int n, int[] parent) {
-    if (isDepthCounted[parent[n]]) {
-      isDepthCounted[n] = true;
-      return depth[parent[n]] + 1;
+  void computeDepths(int[] parent) {
+    for (int i = 1; i < n; i++) {
+      if (!isDepthCounted[i]) {
+        int node = i;
+        int d = 0;
+        while (!isDepthCounted[node]) {
+          node = parent[node];
+          d++;
+        }
+        depth[i] = d + depth[node];
+        isDepthCounted[i] = true;
+      }
     }
-    int r = countDepth(parent[n], parent);
-    return depth[r] + 1;
   }
 
   int getLCA(int a, int b) {
     if (depth[a] < depth[b]) {
-      int c = a;
+      int с = a;
       a = b;
-      b = c;
+      b = с;
     }
 
     int k = depth[a] - depth[b];
@@ -75,32 +85,29 @@ public class LowestCommonAncestor {
   }
 
   void getResults(int a1, int a2) {
-    requestAnswers[1] = getLCA(a1, a2);
+    result += getLCA(a1, a2);
+    int prevAnswer = (int) result;
 
-    requestsData[0] = a1;
-    requestsData[1] = a2;
-
-    for (int i = 2; i < m * 2; i++) {
-      requestsData[i] = getNextNode(requestsData[i - 2], requestsData[i - 1]);
-    }
+    int firstA = getNextNode(a1, a2);
+    int secondA = getNextNode(a2, firstA);
 
     for (int i = 2; i <= m; i++) {
-      int leftV = (requestsData[2 * i - 2] + requestAnswers[i - 1]) % n;
-      requestAnswers[i] = getLCA(leftV, requestsData[2 * i - 1]);
-    }
+      int answer = getLCA((firstA + prevAnswer) % n, secondA);
+      prevAnswer = answer;
 
+      firstA = getNextNode(firstA, secondA);
+      secondA = getNextNode(secondA, firstA);
+
+      result += answer;
+    }
   }
 
   int getNextNode(int e, int f) {
-    return (x * e + y * f + z) % n;
+    return (int) (((long) x * e + (long) y * f + z) % n);
   }
 
-  int getSum() {
-    int s = 0;
-    for (int v : requestAnswers) {
-      s += v;
-    }
-    return s;
+  long getSum() {
+    return result;
   }
 
   public static void main(String[] args) {
@@ -108,26 +115,24 @@ public class LowestCommonAncestor {
     n = sc.nextInt();
     m = sc.nextInt();
 
-//    int n = 4;
-//    int[] parent = new int[]{0, 0, 1, 2};
     int[] parent = new int[n];
     parent[0] = 0;
 
     for (int i = 1; i < n; i++) {
       parent[i] = sc.nextInt();
+      if (parent[i] < 0 || parent[i] >= n) {
+        throw new IllegalArgumentException("Invalid parent index");
+      }
     }
-//    int a1 = sc.nextInt();
-//    int a2 = sc.nextInt();
-//    x = sc.nextInt();
-//    y = sc.nextInt();
-//    z = sc.nextInt();
+
+    int a1 = sc.nextInt();
+    int a2 = sc.nextInt();
+    x = sc.nextInt();
+    y = sc.nextInt();
+    z = sc.nextInt();
 
     LowestCommonAncestor lca = new LowestCommonAncestor(n, parent);
-
-    System.out.println(lca.getLCA(2,3));
-
-//    lca.getResults(a1, a2);
-//    int res = lca.getSum();
-//    System.out.println(res);
+    lca.getResults(a1, a2);
+    System.out.println(lca.getSum());
   }
 }
